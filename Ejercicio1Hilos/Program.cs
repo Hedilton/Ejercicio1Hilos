a34faa153442f3ac3c7012d94ce6466660fbf972
Hedilton <hediltoncolana@gmail.com>
2021-06-11 15:44:22 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio1Hilos/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;//contiene interfaces y clases que definen colecciones genericas,permiten estar tipadas
+using System.Linq;//conjunto de extensiones integradas en c#,permite trabajar mas comodo
+using System.Text;//contiene clases que representan codificaciones de caracteres
+using System.Threading;
+namespace Ejercicio1Hilos
+{
+    class Program
+    {
+            static void Main(string[] args)
+            {
+                Thread x = new Thread(metodo1);
+                Thread y = new Thread(metodo2);
+                x.Start(); y.Start();
+            }
+            static void metodo1()
+            {
+                for (int i = 1; i <= 5; i++)
+                {
+                    Console.WriteLine("Personas ingresando al tren");
+                    Thread.Sleep(500);
+                }
+            }
+            static void metodo2()
+            {
+                for (int i = 1; i <= 5; i++)
+                {
+                    Console.WriteLine("Personas saliendo del tren");
+                    Thread.Sleep(500);
+                }
+            }
+    }
+}
